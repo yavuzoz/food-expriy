f61e945fee888fa8ce4dcaf630911e1a21cb3c08
Yavuz Özbay <ozbayavz@gmail.com>
2024-08-29 16:08:18 +0200

    Initial commit

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,23 +1,17 @@
-// FirebaseConfig.js
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // Firebase Console'dan alınan API Key
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com", // Genellikle gerekli değil, Firebase Console'dan alınan Auth Domain
-  projectId: "food-expriy", // Firebase Console'dan alınan Project ID
-  storageBucket: "food-expriy.appspot.com", // Firebase Console'dan alınan Storage Bucket, genellikle `projectId.appspot.com`
-  messagingSenderId: "757098159822", // Firebase Console'dan alınan Messaging Sender ID
-  appId: "1:757098159822:android:dfe74d1ccbefed9b91fa9f", // Firebase Console'dan alınan App ID
-  measurementId: "" // Genellikle isteğe bağlı, Firebase Console'dan alınan Measurement ID (boş bırakabilirsiniz)
+  apiKey: "AIzaSyCU0a5HOmJldFwU1OGNmHzBi-2RLSz7tVs",
+  authDomain: "food-expriy.firebaseapp.com",
+  projectId: "food-expriy",
+  storageBucket: "food-expriy.appspot.com",
+  messagingSenderId: "757098159822",
+  appId: "1:757098159822:web:794f199f2d3a5dbe91fa9f"
 };
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app();
-}
-
-export default firebase;
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
